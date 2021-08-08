6501009715737d2be6dc70eb6f051e0be1464b1d
swleeee <42002888+swleeee@users.noreply.github.com>
2021-08-08 18:05:39 +0900

    기능 추가 : 리덕스 스토어 예제 연습 및 새로고침 시 css 깨지는 거 해결

--- containers/Home/index.js
+++ containers/Home/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { countPlusAction, countMinusAction } from '../../reducers/count';
 import styled from 'styled-components';
 
 import LoginContainer from './login';
 
 function index(props) {
-  return <LoginContainer />;
+  const dispatch = useDispatch(); // dispatch를 사용하기 쉽게 하는 hook
+  const count = useSelector((state) => state.count); // store의 state를 불러오는 hook   store의 state 중에서 count의 state를 불러온다.
+
+  const onClickPlus = useCallback(() => {
+    // useCallback은 최적화를 위한 hook이다 이 앱에선 굳이 사용 안 해도 되는데 습관이 들면 좋기에 사용.
+    dispatch(countPlusAction());
+  }, []);
+
+  const onClickMinus = useCallback(() => {
+    dispatch(countMinusAction());
+  }, []);
+  return (
+    <div>
+      <LoginContainer />
+      <div style={{ border: '1px solid red' }}>
+        카운트 : {count}
+        <button onClick={onClickPlus}>+</button>
+        <button onClick={onClickMinus}>-</button>
+      </div>
+    </div>
+  );
 }
 
 export default index;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { Provider } from 'react-redux';
+import React from 'react';
+import { createStore, applyMiddleware, compose } from 'redux';
+import withRedux from 'next-redux-wrapper';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from '../reducers/index';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps, store }) => {
+  // return (
+  //   <Provider store={store}>
+  //     <Component {...pageProps} />
+  //   </Provider>
+  // );
 
-export default MyApp
+  return <Component {...pageProps} />;
+};
+
+const configureStore = () => {
+  const middlewares = [];
+  const enhancer =
+    process.env.NODE_ENV === 'production'
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares));
+  // const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
+  const store = createStore(reducer, enhancer);
+  return store;
+};
+
+export default withRedux(configureStore)(MyApp);
+// export default MyApp;
